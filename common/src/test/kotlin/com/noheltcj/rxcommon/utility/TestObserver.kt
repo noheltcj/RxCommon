23240281281cf2333af860977568329552c5d9a7
Colton Nohelty <noheltycolton@gmail.com>
2018-08-26 01:57:16 -0400

    Test observer documentation

--- common/src/test/kotlin/com/noheltcj/rxcommon/utility/TestObserver.kt
+++ common/src/test/kotlin/com/noheltcj/rxcommon/utility/TestObserver.kt
@@ -4,6 +4,14 @@ import com.noheltcj.rxcommon.observers.Observer
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
+/**
+ * An observer for testing *sources*
+ *
+ * Captures events and records them for performing later assertions.
+ *
+ * @param E The type of element this observer can receive
+ * @see Observer
+ */
 class TestObserver<E> : Observer<E> {
   private val nextValues = mutableListOf<E>()
   private var error: Throwable? = null
@@ -11,25 +19,39 @@ class TestObserver<E> : Observer<E> {
   private var disposed = false
 
   /**
-   * asserts that the last value emitted onNext is equal to expected
+   * Asserts that the last value emitted onNext is equal to [expected].
+   *
+   * This method will fail the test if the observer has already terminated with a throwable.
    */
   fun assertValue(expected: E) {
     assertNull(error, "Observer has already been terminated with $error")
     assertEquals(expected, nextValues.lastOrNull())
   }
 
+  /**
+   * Called when the source emits a value. It is not recommended to call this in a test.
+   */
   override fun onNext(value: E) {
     nextValues.add(value)
   }
 
+  /**
+   * Called when the source emits an error. It is not recommended to call this in a test.
+   */
   override fun onError(throwable: Throwable) {
     error = throwable
   }
 
+  /**
+   * Called when the source emits completed. It is not recommended to call this in a test.
+   */
   override fun onComplete() {
     completed = true
   }
 
+  /**
+   * Called when the source emits disposed. It is not recommended to call this in a test.
+   */
   override fun onDispose() {
     disposed = true
   }
