62430d2469b0dbb6de790a38aa12fd197584572f
Colton Nohelty <noheltycolton@gmail.com>
2018-09-23 20:55:09 -0400

    Revert "Updated version"
    This reverts commit b0629e146eb227a39cf739d01a555f459edd65c4.

--- common/src/test/kotlin/com/noheltcj/rxcommon/ObservableIntegrationTests.kt
+++ common/src/test/kotlin/com/noheltcj/rxcommon/ObservableIntegrationTests.kt
@@ -2,6 +2,7 @@ package com.noheltcj.rxcommon
 
 import com.noheltcj.rxcommon.disposables.Disposables
 import com.noheltcj.rxcommon.observables.Observable
+import com.noheltcj.rxcommon.observables.Single
 import com.noheltcj.rxcommon.utility.JsName
 import com.noheltcj.rxcommon.utility.TestObserver
 import kotlin.test.BeforeTest
