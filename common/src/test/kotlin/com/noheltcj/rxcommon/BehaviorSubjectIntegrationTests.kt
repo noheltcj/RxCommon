5e04c5943b7bb93a1001350f10e5a7c78de3e4f6
Colton Nohelty <noheltycolton@gmail.com>
2019-03-02 03:06:50 -0500

    Cleanup

--- common/src/test/kotlin/com/noheltcj/rxcommon/BehaviorSubjectIntegrationTests.kt
+++ common/src/test/kotlin/com/noheltcj/rxcommon/BehaviorSubjectIntegrationTests.kt
@@ -2,7 +2,6 @@ package com.noheltcj.rxcommon
 
 import com.noheltcj.rxcommon.observables.Observable
 import com.noheltcj.rxcommon.subjects.BehaviorSubject
-import com.noheltcj.rxcommon.subjects.PublishSubject
 import com.noheltcj.rxcommon.utility.JsName
 import com.noheltcj.rxcommon.utility.TestObserver
 import kotlin.test.BeforeTest
